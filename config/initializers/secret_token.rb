8044fdd6fed1a82cbdca9a8d15ec24d491733dee
Tuan Nguyen <amtuannguyen@gmail.com>
2025-02-08 13:52:29 -0500

    remove old secret_token

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Eris::Application.config.secret_token = 'cd908840cc7dbfc8c14a19a882cc72c58fcb418c8b2292e96bd7825510e186a9266c290ece1bcecf8e338ac6debbb007fbe65a03ef477b3370049e721f3b54a1'
